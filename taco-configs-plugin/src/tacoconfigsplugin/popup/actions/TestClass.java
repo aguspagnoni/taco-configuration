7bcb91e2404565c2c12e86e73b090682ad86089a
German Rodrigo Romarion <grromarion90@gmail.com>
2016-06-24 18:21:58 -0300

    Added java class to test

---
+++ taco-configs-plugin/src/tacoconfigsplugin/popup/actions/TestClass.java
@@ -0,0 +1,53 @@
+package tacoconfigsplugin.popup.actions;
+
+public class TestClass {
+	
+	public void test_find() throws VizException {
+		setConfigKeyRelevantClasses("ejercicios.AVLTree,ejercicios.AVLNode");
+		setConfigKeyRelevancyAnalysis(true);
+		setConfigKeyCheckNullDereference(true);
+		setConfigKeyCheckArithmeticException(false);
+		setConfigKeyUseJavaArithmetic(true);
+		setConfigKeySkolemizeInstanceInvariant(false);
+		setConfigKeySkolemizeInstanceAbstraction(false);
+		setConfigKeyRemoveQuantifiers(true);
+		// Infer-Scope
+		setConfigKeyInferScope(true);
+		setConfigKeyTypeScopes("ejercicios.AVLTree:1, ejercicios.AVLNode:6");
+		setConfigKeyLoopUnroll(2);
+		// SBP+BOUND
+		setConfigKeyUseJavaSBP(false);
+		setConfigKeyUseTightUpperBounds(false);
+		// JUNIT
+		setConfigKeyGenerateUnitTestCase(true);
+
+		check(GENERIC_PROPERTIES,"find_0", false);
+	}
+
+	public void test_insert() throws VizException {
+		setConfigKeyRelevantClasses("ejercicios.AVLTree,ejercicios.AVLNode");
+		setConfigKeyRelevancyAnalysis(true);
+		setConfigKeyCheckNullDereference(true);
+		setConfigKeyCheckArithmeticException(false);
+		setConfigKeyUseJavaArithmetic(true);
+		setConfigKeySkolemizeInstanceInvariant(false);
+		setConfigKeySkolemizeInstanceAbstraction(false);
+		setConfigKeyRemoveQuantifiers(true);
+		// Infer-Scope
+		setConfigKeyInferScope(true);
+		setConfigKeyTypeScopes("ejercicios.AVLTree:1, ejercicios.AVLNode:3, JavaPrimitiveIntegerValue:7");
+		setConfigKeyLoopUnroll(2);
+		// SBP+BOUND
+		setConfigKeyUseJavaSBP(false);
+		setConfigKeyUseTightUpperBounds(false);
+		// JUNIT
+		setConfigKeyGenerateUnitTestCase(true);
+
+		simulate(GENERIC_PROPERTIES, "insert_0");
+	}
+
+	@Override
+	protected String getClassToCheck() {
+		return "ejercicios.AVLTree";
+	}
+}
