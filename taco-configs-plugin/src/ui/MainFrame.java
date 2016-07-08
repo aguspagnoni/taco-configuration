b84dd983b775fa0ec9ec6242d26336dfff9f81b7
Facundo Alderete <facualderete@gmail.com>
2016-07-08 11:57:54 -0300

    Esqueleto en swing para UI

---
+++ taco-configs-plugin/src/ui/MainFrame.java
@@ -0,0 +1,100 @@
+package ui;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class MainFrame extends JFrame implements WindowListener, ActionListener {
+
+	private final JFileChooser fc = new JFileChooser();
+	private JButton button;
+	private SelectFileDialog selectFileDialog = new SelectFileDialog(this, "Select File Dialog");
+	
+	public static void main(String[] args) {
+		MainFrame mainFrame = new MainFrame("Main Frame");
+		mainFrame.setSize(800, 600);
+		mainFrame.setVisible(true);
+	}
+	
+	public MainFrame(String title) {
+		super(title);
+		setLayout(new FlowLayout());
+		addWindowListener(this);
+		button = new JButton("Click me");
+		add(button);
+		button.addActionListener(this);		
+		selectFileDialog.addConfirmActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO: action to be executed on file select confirmation event
+				System.out.println(fc.getSelectedFile().getName());
+			}
+		});
+		selectFileDialog.setVisible(false);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		int returnVal = fc.showOpenDialog(this);
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			File file = fc.getSelectedFile();
+			// XXX: open the file
+			selectFileDialog.setFileName(file.getName());
+			selectFileDialog.setVisible(true);
+		} else {
+			// XXX: selection cancelled by user
+		}
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		// XXX: close app on main window closing
+		dispose();
+		System.exit(0);
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
