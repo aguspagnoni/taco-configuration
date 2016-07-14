7fe550ef8c2b776f6b6db8159981aa8dccab7057
Federelli <federelli@gmail.com>
2016-07-14 12:14:49 -0300

    Fix Tests.

--- taco-configs-plugin/src/tacoconfigsplugin/popup/actions/ParseConfigurationTest.java
+++ taco-configs-plugin/src/tacoconfigsplugin/popup/actions/ParseConfigurationTest.java
@@ -5,14 +5,15 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import tacoconfigsplugin.popup.actions.Config.ConfigType;
 
 public class ParseConfigurationTest {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		HashMap<String, List<Config>> configs = new ParseConfigurations("/Users/gromarion/Documents/ITBA/AVMC/taco-configuration/taco-configs-plugin/src/tacoconfigsplugin/popup/actions/TestClass.java").configurations();
-		HashMap<String, List<Config>> confs = new HashMap<String, List<Config>>();
+		Map<String, List<Config>> configs = new ParseConfigurations("/Users/gromarion/Documents/ITBA/AVMC/taco-configuration/taco-configs-plugin/src/tacoconfigsplugin/popup/actions/TestClass.java").configurations();
+		Map<String, List<Config>> confs = new HashMap<String, List<Config>>();
 		List<Config> confsList = new ArrayList<Config>();
 		confsList.add(new Config("RelevantClasses", "lalalala", ConfigType.String));
 		confs.put("test_find", confsList);
