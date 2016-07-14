8b67797a4a907dffdfb0819a81ab7b38f1428d05
German Rodrigo Romarion <grromarion90@gmail.com>
2016-07-13 22:14:40 -0300

    Implemented file configurations override method

--- taco-configs-plugin/src/tacoconfigsplugin/popup/actions/ParseConfigurationTest.java
+++ taco-configs-plugin/src/tacoconfigsplugin/popup/actions/ParseConfigurationTest.java
@@ -2,13 +2,21 @@ package tacoconfigsplugin.popup.actions;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+import tacoconfigsplugin.popup.actions.Config.ConfigType;
 
 public class ParseConfigurationTest {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		HashMap<String, List<Config>> configs = new ParseConfigurations("/Users/gromarion/Documents/ITBA/AVMC/taco-configuration/taco-configs-plugin/src/tacoconfigsplugin/popup/actions/TestClass.java").configurations();
+		HashMap<String, List<Config>> confs = new HashMap<String, List<Config>>();
+		List<Config> confsList = new ArrayList<Config>();
+		confsList.add(new Config("RelevantClasses", "lalalala", ConfigType.String));
+		confs.put("test_find", confsList);
+		new ParseConfigurations("/Users/gromarion/Documents/ITBA/AVMC/taco-configuration/taco-configs-plugin/src/tacoconfigsplugin/popup/actions/TestClass.java").setConfigurations(confs);
 		System.out.println(configs);
 	}
 }
--- taco-configs-plugin/src/tacoconfigsplugin/popup/actions/ParseConfigurations.java
+++ taco-configs-plugin/src/tacoconfigsplugin/popup/actions/ParseConfigurations.java
@@ -4,20 +4,25 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+import org.eclipse.ui.commands.KeyConfigurationEvent;
 
 import tacoconfigsplugin.popup.actions.Config.ConfigType;
 
 public class ParseConfigurations {
 	private BufferedReader br;
+	private String filePath;
 	private HashMap<String, List<Config>> configurations = new HashMap<>();
 	public static String testMethodSignature = "public void test_";
 	public static String configMethodName = "setConfigKey";
 	
 	public ParseConfigurations(String filePath) {
 		try {
+			this.filePath = filePath; 
 			br = new BufferedReader(new FileReader(filePath));
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -48,6 +53,43 @@ public class ParseConfigurations {
 			e.printStackTrace();
 		}
 		return configurations;
+	}
+	
+	public void setConfigurations (HashMap<String, List<Config>> configurations) {
+		String line = "";
+		List<String> newContent = new ArrayList<String>();
+		String currentMethodName = "";
+		try {
+			while ((line = br.readLine()) != null) {
+				if (line.contains(testMethodSignature)) {
+					currentMethodName = fetchMethodName(line);
+					newContent.add(line);
+				} else {
+					if (currentMethodName != "") {
+						if (line.contains(configMethodName)) {
+							List<Config> configs = configurations.get(currentMethodName);
+							if (configs != null) {
+								newContent.add(lineToBeWritten(line, configs));							
+							} else {
+								newContent.add(line);
+							}
+						} else {
+							newContent.add(line);
+						}
+					} else {
+						newContent.add(line);
+					}
+				}
+			}
+			PrintWriter writer = new PrintWriter(filePath, "UTF-8");
+			for (String string : newContent) {
+				writer.println(string);				
+			}
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	private String fetchMethodName(String line) {
@@ -98,6 +140,51 @@ public class ParseConfigurations {
 		
 		String[] ans = {configName, configValue, configType};
 		return ans;
+	}
+	
+	private String lineToBeWritten(String line, List<Config> configurations) {
+		String configName = "";
+		String newConfigValue = "";
+		String totalMethodName = "";
+		boolean configNameRead = false;
+		int configValueIndex = -1;
+		for (int i = 0; i < line.length() && !configNameRead; i++) {
+			char c = line.charAt(i);
+			if (totalMethodName.contains(configMethodName))  {
+				if (!configNameRead) {
+					if (c == '(') {
+						configNameRead = true;
+						 newConfigValue = fetchConfigObject(configName, configurations);
+						if (newConfigValue != null) {
+							configValueIndex = i + 1;			
+						}
+					} else {
+						configName += c;
+					}
+				}
+			} else {
+				totalMethodName += c;	
+			}
+		}
+		
+		if (configValueIndex != -1) {
+			return line.substring(0, configValueIndex) + newConfigValue + ");";
+		} else {
+			return line;
+		}
+	}
+	
+	private String fetchConfigObject(String configName, List<Config> configs) {
+		for (Config config : configs) {
+			if (config.name().equals(configName)) {
+				if (config.type().equals(ConfigType.String)) {
+					return "\"" + config.value() + "\"";
+				} else {
+					return config.value();
+				}
+			}
+		}
+		return null;
 	}
 	
 	boolean isIntegerClass(String value) {
